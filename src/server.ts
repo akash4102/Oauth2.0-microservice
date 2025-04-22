8b00f860f94d3e200747b768ac465f7525492e70
trackier <aakashverma4102@gmail.com>
2025-04-22 23:00:45 +0530

    configure package.json file

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import app from './app';
-import { env } from './config/env';
+import app from "./app";
+import { env } from "./config/env";
 
 app.listen(env.PORT, () => {
   console.log(`🚀 Server running on http://localhost:${env.PORT}`);
